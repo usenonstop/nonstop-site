b1fe1b5ff1632f1cc8200755324f6651c10a31a8
sinuheshinbr <sinuheshin@gmail.com>
2024-02-21 18:05:18 -0300

    feat: add link to token

--- src/ui/NoToken.tsx
+++ src/ui/NoToken.tsx
@@ -1,10 +1,20 @@
+import Link from "next/link";
 import { FaArrowUp } from "react-icons/fa";
 
 export const NoToken = () => (
-  <div className="flex items-center gap-2">
-    <span>
+  <div className="flex flex-col items-center">
+    <div>
+      Pegue sem token em:{" "}
+      <Link
+        className="underline"
+        href="https://www.usenonstop.com/preferencias"
+      >
+        https://www.usenonstop.com/preferencias
+      </Link>
+    </div>
+    <div className="flex items-center justify-center gap-2">
       Inclua seu token nonStop ali em cima para fazer a mágica acontecer
-    </span>
-    <FaArrowUp className="rotate-45 transform" />
+      <FaArrowUp className="rotate-45 transform" />
+    </div>
   </div>
 );
